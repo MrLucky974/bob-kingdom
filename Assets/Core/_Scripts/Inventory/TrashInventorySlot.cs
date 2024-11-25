75c9e45e7cd096faea5882c693695309e02f5d57
Luckius_ <herocreepyfr@gmail.com>
2024-11-25 11:24:09 +0400

    Add trash can inventory slot

---
+++ Assets/Core/_Scripts/Inventory/TrashInventorySlot.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TrashInventorySlot : MonoBehaviour, IDropHandler
+{
+    private const float COST_MULTIPLIER = 0.5f;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject droppedObject = eventData.pointerDrag;
+        if (droppedObject.TryGetComponent<DraggableItem>(out _))
+        {
+            var currentItemCost = Player.Instance.CurrentItemCost;
+            Player.Instance.GiveMoney(Mathf.RoundToInt(currentItemCost * COST_MULTIPLIER));
+            Destroy(droppedObject);
+        }
+    }
+}
